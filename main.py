2e8811abddbaee90ffbe253620271386e46e5121
dn28203 <ntkngann2k3@gmail.com>
2025-02-24 13:21:24 +0700

    test pcl

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 
 
-class Reconstruction: 
-    def __init__(self, images):
-        self.images = images
-        self.depth = None   
-        self.pcl = None
-    def show(self):
-        pass
-    def save_obj(self, path):
-        pass
-    def create_point_cloud(self):
-        pass
-    def create_depth_image(self):
-        pass
-    def create_mesh(self):
-        pass
-    def add_texture(self, texture):
-        pass
+# class Reconstruction: 
+#     def __init__(self, images):
+#         self.images = images
+#         self.depth = None   
+#         self.pcl = None
+#     def show(self):
+#         pass
+#     def save_obj(self, path):
+#         pass
+#     def create_point_cloud(self):
+#         pass
+#     def create_depth_image(self):
+#         pass
+#     def create_mesh(self):
+#         pass
+#     def add_texture(self, texture):
+#         pass
 
+# from diffusers import DiffusionPipeline, ControlNetModel
+from transformers import SiglipImageProcessor
